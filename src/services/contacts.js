a5925f70cec81dee750671c2d12efd44e33aca74
dmitrmedv <dmitrmedv@gmail.com>
2025-01-24 16:32:27 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,7 +2,6 @@ import { ContactsCollection } from '../db/models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
-  console.log('Contacts fetched:', contacts);
   return contacts;
 };
 
