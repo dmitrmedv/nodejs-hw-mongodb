785625393c53f50827966907a6267e36c43919a1
dmitrmedv <dmitrmedv@gmail.com>
2025-01-24 16:45:40 +0200

    fix

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -4,6 +4,7 @@ import {
   getAllContactsController,
   getContactByIdController,
   patchContactController,
+  deleteContactController,
 } from '../controllers/contacts.js';
 
 const router = Router();
@@ -13,4 +14,7 @@ router.get('/contacts', ctrlWrapper(getAllContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
 
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
 export default router;
